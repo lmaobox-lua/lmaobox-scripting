869ac83b4885ed45d4447e2e2abf49acc5067066
I fucking hate Rap <64323552+LewdDeveloper@users.noreply.github.com>
2022-08-28 02:40:09 +0700

    ???

--- menu_user.lua
+++ menu_user.lua
@@ -6,7 +6,7 @@ local header = main:label('yes', 2)
 main:label('?')
 -- printLuaTable(main)
 callbacks.Register( 'Draw', function()
-    if input.IsButtonPressed( KEY_L ) then
+    if input.IsButtonDown( KEY_L ) then
         input.SetMouseInputEnabled( true )
         main:text( table.concat( input.GetMousePos(), ', ' ) )
         header:text( menu.get_held_time( KEY_L ) )
